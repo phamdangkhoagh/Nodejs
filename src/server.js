0fc108e6720bde23c0a25876385d607f9ec00995
Pham Dang Khoa <156926024+phamdangkhoagh@users.noreply.github.com>
2024-08-09 22:30:44 +0700

    add files

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('dotenv').config();
 const  configViewEngine = require('./config/viewEngine');
 const webRouter = require('./routes/web');
+const connection = require('./config/database');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -13,6 +15,25 @@ configViewEngine(app);
 //Khai báo route
 app.use('/',webRouter);
 
-app.listen(port, host, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+const cat = new Kitten({ name: 'Hi Khoa' });
+cat.save();
+
+//test connection
+(async() => {
+  //test connection 
+  try {
+    await connection().then;
+    app.listen(port, host, () => {
+      console.log(`Backend zero listening on port ${port}`)
+    })
+  } catch(error){
+    console.log("Error connect to DB: ",error);
+  }
+
+})()
+
